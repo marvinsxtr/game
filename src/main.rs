48393623ec9b8daaef1f3bfe91a62c8a4cff78ac
Marvin Sextro <marvin.sextro@gmail.com>
2020-09-14 18:52:16 +0200

    Hello world spam

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_system(hello_world.system()).run();
+    App::build()
+        .add_default_plugins()
+        .add_plugin(HelloPlugin)
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello world!");
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(HelloWorldTimer(Timer::from_seconds(2.0, true)))
+            .add_system(hello_world.system());
+    }
+}
+
+pub struct HelloWorldTimer(Timer);
+
+fn hello_world(time: Res<Time>, mut timer: ResMut<HelloWorldTimer>) {
+    timer.0.tick(time.delta_seconds);
+
+    if timer.0.finished {
+        println!("Hello world!");
+    }
 }

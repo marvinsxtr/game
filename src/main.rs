9b103a53880e4833864323e0808df05a8f3773c0
Marvin Sextro <marvin.sextro@gmail.com>
2021-04-13 18:19:39 +0200

    Restructure player plugin

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,28 @@ use bevy:: {
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
 };
-use player::{Player, PlayerPlugin};
-mod player;
+use plugins::player::PlayerPlugin;
+mod plugins {
+    pub mod player;
+}
 
 fn main() {
     App::build()
+        .add_plugins(DefaultPlugins)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(PlayerPlugin)
+        .add_startup_system(setup.system())
+        .insert_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
         .insert_resource(bevy::log::LogSettings {
-            level: bevy::log::Level::DEBUG,
+            level: bevy::log::Level::INFO,
             ..Default::default()
         })
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_plugin(PlayerPlugin)
-        .insert_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
         .run();
 }
 
 fn setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    _asset_server: Res<AssetServer>,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-            material: materials.add(Color::rgb(0.2, 0.5, 0.5).into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
-            ..Default::default()
-        })
-        .insert(Player { speed: 500.0 });
 }
--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -1,15 +1,27 @@
 use bevy::prelude::*;
-
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(player_movement.system());
+        app.add_startup_system(player_setup.system())
+           .add_system(player_movement.system());
     }
 }
-
 pub struct Player {
     pub speed: f32,
+}
+
+fn player_setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 0.5, 0.5).into()),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+        ..Default::default()
+    })
+    .insert(Player { speed: 500.0 });
 }
 
 fn player_movement(

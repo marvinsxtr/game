6ad37d6deaae28e02691308b937c4d41ecb67241
Marvin Sextro <marvin.sextro@gmail.com>
2021-04-13 23:39:51 +0200

    Add projectiles

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy:: {
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
 };
+use heron::PhysicsPlugin;
 use plugins::player::PlayerPlugin;
 mod plugins {
     pub mod player;
@@ -12,18 +13,17 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(PhysicsPlugin::default())
         .add_plugin(PlayerPlugin)
-        .add_startup_system(setup.system())
         .insert_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
         .insert_resource(bevy::log::LogSettings {
             level: bevy::log::Level::INFO,
             ..Default::default()
         })
+        .add_startup_system(setup.system())
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -1,14 +1,18 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::Camera};
+use heron::prelude::*;
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(player_setup.system())
-           .add_system(player_movement.system());
+           .add_system(player_movement.system())
+           .add_system(player_weapon.system());
     }
 }
+
 pub struct Player {
     pub speed: f32,
+    pub direction: Quat,
 }
 
 fn player_setup(
@@ -17,43 +21,99 @@ fn player_setup(
 ) {
     commands.spawn_bundle(SpriteBundle {
         material: materials.add(Color::rgb(0.2, 0.5, 0.5).into()),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+        transform: Transform::from_xyz(0., 0., 0.),
+        sprite: Sprite::new(Vec2::new(30., 30.)),
         ..Default::default()
     })
-    .insert(Player { speed: 500.0 });
+    .insert(Player { speed: 500. , direction: Quat::IDENTITY});
 }
 
 fn player_movement(
     time: Res<Time>,
+    windows: Res<Windows>,
+    cam_query: Query<&Camera>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform)>,
+    mut query: Query<(&mut Player, &mut Transform)>,
+    mut evr_cursor: EventReader<CursorMoved>,
 ) {
-    for (player, mut transform) in &mut query.iter_mut() {
+    // Get the camera and the player
+    let camera: &Camera = cam_query.single().unwrap();
+    let (mut player, mut transform) = query.single_mut().unwrap();
 
-        let arrow_keys: Vec<KeyCode> = [
-            KeyCode::Left, 
-            KeyCode::Right, 
-            KeyCode::Up, 
-            KeyCode::Down
-        ].into();
+    // Get the screen position of the player
+    let player_screen_pos: Vec2 = camera
+        .world_to_screen(&windows, &GlobalTransform::identity(), transform.translation)
+        .unwrap();
 
-        for key_code in arrow_keys {
-            if !keyboard_input.pressed(key_code) { continue };
+    // Get change of cursor
+    match evr_cursor.iter().last() {
+        Some(cursor_screen_pos) => {
+            // Calculate the angle of the player towards the cursor
+            let a: f32 = player_screen_pos.x - cursor_screen_pos.position.x;
+            let b: f32 = player_screen_pos.y - cursor_screen_pos.position.y;
+
+            let angle: f32 = b.atan2(a);
             
-            transform.translation.x += time.delta_seconds() * player.speed * 
-                match key_code {
-                    KeyCode::Left => -1.0,
-                    KeyCode::Right => 1.0,
-                    _ => { 0f32 }
-                };
+            // Adjust player direction
+            player.direction = Quat::from_rotation_z(angle);
+        },
+        _ => (),
+    };
 
-            transform.translation.y += time.delta_seconds() * player.speed * 
-                match key_code {
-                    KeyCode::Up => 1.0,
-                    KeyCode::Down => -1.0,
-                    _ => { 0f32 }
-                };
-        }
+    // Handle player movement
+    let arrow_keys: Vec<KeyCode> = [
+        KeyCode::A, 
+        KeyCode::D, 
+        KeyCode::W, 
+        KeyCode::S
+    ].into();
+
+    for key_code in arrow_keys {
+        if !keyboard_input.pressed(key_code) { continue };
+        
+        transform.translation.x += time.delta_seconds() * player.speed * 
+            match key_code {
+                KeyCode::A => -1.,
+                KeyCode::D => 1.,
+                _ => { 0. }
+            };
+
+        transform.translation.y += time.delta_seconds() * player.speed * 
+            match key_code {
+                KeyCode::W => 1.,
+                KeyCode::S => -1.,
+                _ => { 0. }
+            };
+    }
+}
+
+fn player_weapon(
+    keyboard_input: Res<Input<KeyCode>>,
+    query: Query<(&Player, &Transform)>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Get the player
+    let (player, transform) = query.single().unwrap();
+
+    // Shoot when space is pressed
+    if keyboard_input.pressed(KeyCode::Space) {
+
+        // Spawn the projectile
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(Color::rgb(0.7, 0., 0.).into()),
+                transform: Transform {
+                    translation: transform.translation,
+                    rotation: player.direction,
+                    ..Default::default()
+                },
+                sprite: Sprite::new(Vec2::new(50., 10.)),
+                ..Default::default()
+            })
+            .insert(GlobalTransform::identity())
+            .insert(Body::Sphere { radius: 10.0 })
+            .insert(BodyType::Kinematic)
+            .insert(Velocity::from(-player.direction.mul_vec3(Vec3::X) * 1000.));
     }
 }
